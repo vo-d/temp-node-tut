13573dc0957101e4def57277021e57c2bf702e35
vo-d <daidaivo2002@gmail.com>
2022-07-07 20:42:17 -0500

    add semicolon

--- 12-http.js
+++ 12-http.js
@@ -4,17 +4,17 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
         console.log(req);
-        res.end('Welcome to our home page')
+        res.end('Welcome to our home page');
     }
     if(req.url === '/about'){
-        res.end('Here is our short history')
+        res.end('Here is our short history');
     }
     else{
         res.end(`\
         <h1>Oops!<h1>
         <p>We can't seem to find the page you are looking for<p>
         <a href="/">back home</a>
-        `)
+        `);
     }
     
 })
